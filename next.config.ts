1045f37da32049c84f2976d13ae244724a768d1f
Awes0314 <Awes31415@gmail.com>
2025-07-18 17:24:26 +0900

    api対応

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  output: undefined,
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
